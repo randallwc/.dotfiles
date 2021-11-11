9b6f297bfb9ecdfa6d9360a8412c01d9067f4dc8
William Randall <15082470+randallwc@users.noreply.github.com>
2021-11-10 22:03:39 -0800

    spacing

--- setup.sh
+++ setup.sh
@@ -1,41 +1,42 @@
 #!/usr/bin/env bash
 files="vimrc zshrc zprofile bashrc profile gitconfig gitignore_global"
-repositories="CSM152A CSM148 randallwc randallwc.github.io"
+repositories="CSM152A CSM148 CSM148.3 randallwc randallwc.github.io"
 
 echo "making simlinks in home dir"
 for file in $files;
 do
-	if [[ "$1" == "--force" ]]
-	then
-		echo "force rewrite of simlinks for file $file"
-		ln -fs ~/.dotfiles/$file ~/.$file
-	else
-		ln -s ~/.dotfiles/$file ~/.$file
-	fi
+    if [[ "$1" == "--force" ]]
+    then
+        echo "force rewrite of simlinks for file $file"
+        ln -fs ~/.dotfiles/$file ~/.$file
+    else
+        ln -s ~/.dotfiles/$file ~/.$file
+    fi
 done
 
 if [[ ! -d ~/Github/ ]]
 then
-	if [[ "$1" == "--github" ]]
-	then
-		echo "making GitHub in home directory"
-		mkdir ~/GitHub/
-		cd ~/GitHub/
-		git --version >/dev/null 2>&1
-		if [[ $? -ne 0 ]]
-		then
-			echo "git not installed"
-			exit 1
-		fi
-		for repo in $repositories
-		do
-			git clone git@github.com:randallwc/$repo
-			if [[ $? -ne 0 ]]
-			then
-				echo "error occured in git clone"
-				rmdir ~/GitHub
-				exit 1
-			fi
-		done
-	fi
+    if [[ "$1" == "--github" ]]
+    then
+        echo "making GitHub in home directory"
+        mkdir ~/GitHub/
+        cd ~/GitHub/
+        git --version >/dev/null 2>&1
+        if [[ $? -ne 0 ]]
+        then
+            echo "git not installed"
+            exit 1
+        fi
+        for repo in $repositories
+        do
+            echo "cloning $repo ..."
+            git clone git@github.com:randallwc/$repo
+            if [[ $? -ne 0 ]]
+            then
+                echo "error occured in git clone"
+                rmdir ~/GitHub
+                exit 1
+            fi
+        done
+    fi
 fi
