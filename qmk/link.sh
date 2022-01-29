f19b57b7eb5a1a13fd43b9664685200f46e52229
William Randall <15082470+randallwc@users.noreply.github.com>
2022-01-28 18:18:21 -0600

    move qmk things in dotfiles

---
+++ qmk/link.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env sh
+files="willDefault willSnail"
+
+for file in $files
+do
+    # ln -s [actual file] [link location]
+    if ln -s ~/.dotfiles/$file ~/GitHub/qmk_firmware/keyboards/nullbitsco/nibble/keymaps/$file
+    then
+        echo "DEBUG: successfully linked $file"
+    else
+        echo "FAIL: did not link $file"
+    fi
+done
+unset files
