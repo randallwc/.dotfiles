eebc7407a42ed0816d68da852399990997e810b9
William Randall <15082470+randallwc@users.noreply.github.com>
2022-01-28 18:20:26 -0600

    fix link.sh

--- qmk/link.sh
+++ qmk/link.sh
@@ -4,7 +4,7 @@ files="willDefault willSnail"
 for file in $files
 do
     # ln -s [actual file] [link location]
-    if ln -s ~/.dotfiles/$file ~/GitHub/qmk_firmware/keyboards/nullbitsco/nibble/keymaps/$file
+    if ln -s ~/.dotfiles/qmk/$file ~/GitHub/qmk_firmware/keyboards/nullbitsco/nibble/keymaps/$file
     then
         echo "DEBUG: successfully linked $file"
     else
