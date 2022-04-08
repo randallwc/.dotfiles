4f5f10f7dc84815d899954c2cf7c622d33af7e92
William Randall <15082470+randallwc@users.noreply.github.com>
2022-04-08 01:17:09 -0700

    format and lint setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 files="vimrc zshrc zprofile bashrc profile gitconfig gitignore_global condarc bash_profile ideavimrc"
-repositories="randallwc.github.io 180DA-WarmUp CSCM121"
+repositories="randallwc.github.io 180DA-WarmUp"
 
 echo "making simlinks in home dir"
-for file in $files;
-do
-    if [[ "$1" == "--force" ]]
-    then
+for file in $files; do
+    if [[ "$1" == "--force" ]]; then
         echo "force rewrite of simlinks for file $file"
-        ln -fs ~/.dotfiles/$file ~/.$file
+        ln -fs ~/.dotfiles/"$file" ~/."$file"
     else
-        if ln -s ~/.dotfiles/$file ~/.$file
-        then
+        if ln -s ~/.dotfiles/"$file" ~/."$file"; then
             echo "SUCCESS: linked file $file"
         else
             echo "FAIL: was not able to link $file"
@@ -21,25 +18,18 @@ done
 unset file
 
 # if github dir does not exist
-if [[ "$1" == "--github" ]]
-then
-    if [[ ! -d ~/Github/ ]]
-    then
+if [[ "$1" == "--github" ]]; then
+    if [[ ! -d ~/Github/ ]]; then
         echo "making GitHub in home directory"
         mkdir ~/GitHub/
-        cd ~/GitHub/
-        git --version >/dev/null 2>&1
-        if [[ $? -ne 0 ]]
-        then
+        cd ~/GitHub/ || exit 1
+        if git --version >/dev/null 2>&1; then
             echo "FAIL: git not installed"
             exit 1
         fi
-        for repo in $repositories
-        do
+        for repo in $repositories; do
             echo "DEBUG: cloning $repo ..."
-            git clone git@github.com:randallwc/$repo
-            if [[ $? -ne 0 ]]
-            then
+            if git clone git@github.com:randallwc/"$repo"; then
                 echo "FAIL: error occured in git clone"
                 rmdir ~/GitHub
                 exit 1
