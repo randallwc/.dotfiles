bd08aca8b973b2828a1a2f4feb08b34f74ba8f57
William Randall <15082470+randallwc@users.noreply.github.com>
2022-04-13 16:43:53 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,29 @@
 files="vimrc zshrc zprofile bashrc profile gitconfig gitignore_global condarc bash_profile ideavimrc pylintrc"
 repositories="randallwc.github.io 180DA-WarmUp"
 
-echo "making simlinks in home dir"
+colorprint() {
+    case $2 in
+        blue) TEMP_COLOR="\033[0;34m" ;;
+        red) TEMP_COLOR="\033[0;31m" ;;
+        green) TEMP_COLOR="\033[0;32m" ;;
+        dgray) TEMP_COLOR="\033[1;30m" ;;
+        lgray) TEMP_COLOR="\033[0;37m" ;;
+        *) echo "invalid color" && exit 1 ;;
+    esac
+    echo -e "${TEMP_COLOR}$1\033[0m"
+    unset TEMP_COLOR
+}
+
+colorprint "making simlinks in home dir" dgray
 for file in $files; do
     if [[ "$1" == "--force" ]]; then
-        echo "force rewrite of simlinks for file $file"
+        colorprint "FORCED: link $file to ~/.dotfiles/$file" blue
         ln -fs ~/.dotfiles/"$file" ~/."$file"
     else
-        if ln -s ~/.dotfiles/"$file" ~/."$file"; then
-            echo "SUCCESS: linked file $file"
+        if ln -s ~/.dotfiles/"$file" ~/."$file" 2> /dev/null; then
+            colorprint "SUCCESS: linked $file to ~/.dotfiles/$file" green
         else
-            echo "FAIL: was not able to link $file"
+            colorprint "[$?] FAIL: on linking $file to ~/.dotfiles/$file" red
         fi
     fi
 done
@@ -20,24 +33,24 @@ unset file
 # if github dir does not exist
 if [[ "$1" == "--github" ]]; then
     if [[ ! -d ~/Github/ ]]; then
-        echo "making GitHub in home directory"
+        colorprint "making GitHub in home directory" dgray
         mkdir ~/GitHub/
         cd ~/GitHub/ || exit 1
         if git --version >/dev/null 2>&1; then
-            echo "FAIL: git not installed"
+            colorprint "FAIL: git not installed" red
             exit 1
         fi
         for repo in $repositories; do
-            echo "DEBUG: cloning $repo ..."
+            colorprint "DEBUG: cloning $repo ..." dgray
             if git clone git@github.com:randallwc/"$repo"; then
-                echo "FAIL: error occured in git clone"
+                colorprint "FAIL: error occured in git clone" red
                 rmdir ~/GitHub
                 exit 1
             fi
         done
         unset repo
     else
-        echo "FAIL: ~/Github/ exists"
+        colorprint "FAIL: ~/Github/ exists" red
     fi
 fi
 
