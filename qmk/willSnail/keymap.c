0cd56cd9a6f7a9107870f0030db42a865b866caf
William Randall <15082470+randallwc@users.noreply.github.com>
2023-05-29 20:18:19 -0700

    update qmk configs

--- qmk/willSnail/keymap.c
+++ qmk/willSnail/keymap.c
@@ -26,18 +26,20 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_MA] = LAYOUT_ansi(
-                 KC_GESC,    KC_1, KC_2,      KC_3, KC_4,       KC_5, KC_6,     KC_7, KC_8,        KC_9, KC_0,    KC_MINS, KC_EQL,           KC_BSPC, KC_DEL,
-        KC_MUTE, KC_TAB,     KC_Q, KC_W,      KC_E, KC_R,       KC_T, KC_Y,     KC_U, KC_I,        KC_O, KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_PGUP,
-        KC_CAPS, MO(1),      KC_A, KC_S,      KC_D, KC_F,       KC_G, KC_H,     KC_J, KC_K,        KC_L, KC_SCLN, KC_QUOT, KC_ENT,           KC_PGDN,
-        KC_BRIU, KC_LSFT,    KC_Z, KC_X,      KC_C, KC_V,       KC_B, KC_N,     KC_M, KC_COMM,   KC_DOT, KC_SLSH, KC_RSFT, KC_UP,   LGUI(LCTL(KC_Q)),
-        KC_BRID, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_RGUI, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
-    [_FN] = LAYOUT_ansi(
-                 KC_GRV,    KC_F1, KC_F2,     KC_F3, KC_F4,     KC_F5, KC_F6,     KC_F7, KC_F8,      KC_F9, KC_F10,   KC_F11, KC_F12,   KC_F13, KC_F14,
-        RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, KC_HOME,
-        _______, _______, _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT, KC_END,  _______, _______,  KC_END,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV,  KC_MNXT, KC_MPLY, _______, _______,  KC_PWR,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)
+  [_MA] = LAYOUT_ansi(
+             QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV,
+    KC_MUTE, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
+    MO(_FN), KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+    KC_BRIU, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
+    KC_BRID, KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                    KC_RGUI, KC_RALT, MO(_FN), KC_LEFT,          KC_DOWN, KC_RGHT
+  ),
+  [_FN] = LAYOUT_ansi(
+             _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, _______,
+    RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,          _______, KC_HOME,
+    _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, _______, _______, _______, _______, _______, _______, _______,          _______,  KC_END,
+    _______, _______, _______, _______,                   _______,                   _______, _______, _______, _______,          _______, _______
+  ),
 };
 
 #ifdef OLED_ENABLE
